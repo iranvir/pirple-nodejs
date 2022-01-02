8bb7eb05192ed1fc49b857a4348bef3e51492987
Ranvir Singh <sranvir155@gmail.com>
2022-01-02 18:55:30 +0000

    PUT implemented

--- 3-Building-a-RESTful-API/lib/handlers.js
+++ 3-Building-a-RESTful-API/lib/handlers.js
@@ -79,7 +79,6 @@ handlers._users.post = function(data,callback) {
 // @TODO Only let an authenticated user acces their object. Don't let them access anyone else's 
 handlers._users.get = function(data,callback) {
     // Check that the phone number is valid
-    console.log(data.queryStringObject.phone.trim());
     var phone = typeof(data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone.trim() : false;
     if(phone){
         // Lookup the user
@@ -97,14 +96,60 @@ handlers._users.get = function(data,callback) {
     }
 };
 
-//Users - put
+// Users - put
+// Requried data: phone
+// Optional data: firstName, lastName, password (at least one must be specified)
+// @TODO Only let an authenticated user update their own object. Don't let them update anyone else's
 handlers._users.put = function(data,callback) {
-    
+    // Check for the required field, i.e, phone
+    var phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
+    console.log(phone);
+    // Check for the optional fields
+    var firstName = typeof(data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
+    var lastName = typeof(data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
+    var password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+    var tosAgreement = typeof(data.payload.tosAgreement) == 'boolean' && data.payload.tosAgreement == true ? true : false;
+
+    // Error if the phone is invalid
+    if(phone) {
+        if(firstName || lastName || password) {
+            // Lookup the user
+            _data.read('users', phone, function(err, userData) {
+                if(!err && userData) {
+                    // Update the fields necessary
+                    if(firstName){
+                        userData.firstName = firstName;
+                    }
+                    if(lastName){
+                        userData.lastName = lastName;
+                    }
+                    if(password){
+                        userData.hasedPassword = helpers.hash(password);
+                    }
+                    // Store the new updates
+                    _data.update('users',phone,userData,function(err) {
+                        if(!err) {
+                            callback(200);
+                        } else {
+                            console.log(err);
+                            callback(500,{'Error':'Could not update the user'});
+                        }
+                    });
+                } else {
+                    callback(400,{'Error':'The specified user doesn\'t exist'});
+                }
+            })
+        } else {
+            callback(400, {'Error':'Missing fields to update'});
+        }
+    } else {
+        callback(400, {'Error':'Missing the required field'});
+    }
 };
 
-//Users - delete
+// Users - delete
 handlers._users.delete = function(data,callback) {
-    
+
 };
 
 // Ping handler
