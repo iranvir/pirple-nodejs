893f179d5ae4f66215353bb3ed24f647719ab80a
Ranvir Singh <sranvir155@gmail.com>
2022-01-04 09:03:11 +0000

    Started working on tokens

--- 3-Building-a-RESTful-API/lib/handlers.js
+++ 3-Building-a-RESTful-API/lib/handlers.js
@@ -173,6 +173,121 @@ handlers._users.delete = function(data,callback) {
     }
 };
 
+// Tokens
+handlers.tokens = function(data,callback){
+    var acceptableMethods = ['post', 'get','put','delete'];
+    if(acceptableMethods.indexOf(data.method) > -1 ){
+        handlers._tokens[data.method](data,callback);
+    } else {
+        callback(405);
+    }
+};
+
+// Container for all the token methods
+handlers._tokens = {};
+
+// Tokens - post
+// Required data: phone, password
+// Optional data: none
+handlers._tokens.post = function(data, callback){
+    var phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
+    var password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+    if(phone && password) {
+        // Lookup the user who matches that phone number
+        _data.read('users',phone,function(err,userData){
+            if(!err && userData){
+                // Hash the sent password and compare it to the password stored in the user object
+                var hashedPassword = helpers.hash(password);
+                if(hashedPassword == userData.hashedPassword){
+                    // If valid, create a new token with a random name. Set expiration date 1 hour in the future
+                    var tokenId = helpers.createRandomString(20);
+                    var expires = Date.now() + 1000 * 60 * 60 ; // 1000 is used because the unit is milliseconds
+                    var tokenObject = {
+                        'phone':phone,
+                        'id': tokenId,
+                        'expires':expires
+                    };
+                    
+                    // Store the token
+                    _data.create('tokens',tokenId,tokenObject,function(err){
+                        if(!err){
+                            callback(200,tokenObject);
+                        } else {
+                            callback(500,{'Error':'Could not create the new token'});
+                        }
+                    });
+                } else {
+                    callback(400,{'Error': 'Password did not match the specified user\'s stored password'});
+                }
+            } else {
+                callback(400,{'Error':'Could not find the specified user'});
+            }
+        });
+    } else {
+        callback(400, {'Error':'Missing required field(s)'});
+    }
+};
+
+// Tokens - get
+// Required data: id
+// Optional data: None
+handlers._tokens.get = function(data, callback){
+    // Check that the id is valid
+    var id = typeof(data.queryStringObject.id) == 'string' && data.queryStringObject.id.trim().length == 20 ? data.queryStringObject.id.trim() : false;
+    if(id){
+        // Lookup the user
+        _data.read('tokens',id,function(err,tokenData){
+            if(!err && tokenData){
+                callback(200,tokenData);
+            } else {
+                callback(404);
+            }
+        });
+    } else {
+        callback(400,{'Error': 'Missing required fields'});
+    }
+};
+
+// Tokens - put
+// Required data: id, extend
+handlers._tokens.put = function(data, callback){
+    var phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
+    var extend = typeof(data.payload.phone) == 'boolean' && data.payload.extend == true ? true : false;
+    if(id && extend){
+        // Lookup the token
+        _data.read('tokens',id, function(err,tokenData){
+            if(!err && tokenData){
+                // Check to make sure that the token isn't already expired
+                if(tokenData.expires > Date.now()){
+                    // Set the expiration an hour from now
+                    tokenData.expires = Date.now() + 1000 * 60 * 60 ;
+
+                    // Store the new updates
+                    _data.update('tokens',id,tokenData,function(err){
+                        if(!err){
+                            callback(200);
+                        } else {
+                            callback(500,{'Error':'Could not update the token\'s expiration'});
+                        }
+                    });
+                } else {
+                    callback(400,{'Error':'The token has already expired, and cannot be extended'});
+                }
+            } else {
+                callback(400,{'Error':'Specified token doesn\'t exist'});
+            }
+        });
+    } else {
+        callback(400,{'Error':'Missing required field(s) or field(s) are invalid'});
+    }
+};
+
+// Tokens - delete
+handlers._tokens.delete = function(data, callback){
+
+};
+
+
 // Ping handler
 handlers.ping = function(data, callback){
     callback(200);
@@ -183,4 +298,4 @@ handlers.notFound = function(data,callback){
 };
 
 // Export the module
-module.exports = handlers; 
+module.exports = handlers;
--- 3-Building-a-RESTful-API/lib/helpers.js
+++ 3-Building-a-RESTful-API/lib/helpers.js
@@ -29,5 +29,26 @@ helpers.parseJsonToObject = function(str){
     }
 };
 
+// Create a string of random alphanumeric characters of a given length
+helpers.createRandomString = function(strLength){
+    strLength = typeof(strLength) == 'number' && strLength > 0 ? strLength : false;
+    if(strLength){
+        // Define all the possible characters that could go into a string
+        var possibleCharacter = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+        // Start the final string
+        var str = '';
+        for(i = 1; i<= strLength; i++){
+            // Get a random character from the possibleCharacters string
+            var randomCharacter = possibleCharacter.charAt(Math.floor(Math.random() * possibleCharacter.length));
+            // Applend this to the final string
+            str+=randomCharacter;
+        }
+        // Return the final string
+        return str;
+    } else {
+        return false;
+    }
+};
 // Export the module
 module.exports = helpers;
